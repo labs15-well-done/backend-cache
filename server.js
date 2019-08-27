0c46c7e670ba02b297dc000feee1268092f2d4ad
Joshua Wood <JerichoWall@apu.edu>
2019-08-26 17:29:55 -0700

    Added cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const sensorRouter = require('./sensor_endpoints/sensor-router.js');
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 
 server.use('/p-api', sensorRouter);
 
